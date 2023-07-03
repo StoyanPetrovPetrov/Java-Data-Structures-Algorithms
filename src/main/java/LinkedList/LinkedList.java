9ce4dc7dda19376340bfa05a4650ff4a8df07220
Stoyan Petrov <Petrov2022@hotmail.com>
2023-07-03 18:21:37 +0300

    new

---
+++ src/main/java/LinkedList/LinkedList.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node{
+        int value;
+        Node text;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+    public LinkedList(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+}
