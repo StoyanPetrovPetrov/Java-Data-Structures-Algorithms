29c18e2d200a48a9e23925ccbb4d1fe9740a6416
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-07-05 18:26:06 +0300

    Add files via upload

---
+++ src/main/java/LinkedList/removeFirst/Main.java
@@ -0,0 +1,18 @@
+package LinkedList.removeFirst;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(2);
+        myLinkedList.append(1);
+
+        // (2) Items - Returns 2 Node
+        System.out.println(myLinkedList.removeFirst().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myLinkedList.removeFirst().value);
+        // (0) Items - Returns null
+        System.out.println(myLinkedList.removeFirst());
+
+    }
+
+}
