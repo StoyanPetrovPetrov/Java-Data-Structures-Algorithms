3ad22585161eb5804accd9642121ceca9757150c
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-07-10 17:53:49 +0300

    Add files via upload

---
+++ src/main/java/DoubleLinkedList/get/Main.java
@@ -0,0 +1,17 @@
+package DoubleLinkedList.get;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(0);
+        myDLL.append(1);
+        myDLL.append(2);
+        myDLL.append(3);
+
+
+        System.out.println(myDLL.get(3).value);
+
+
+
+    }
+}
