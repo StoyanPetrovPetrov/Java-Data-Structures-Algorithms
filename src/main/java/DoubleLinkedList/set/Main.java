dbf4370cdca4a5e7200274bf165cef94e200fc90
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-07-10 17:56:10 +0300

    Add files via upload

---
+++ src/main/java/DoubleLinkedList/set/Main.java
@@ -0,0 +1,24 @@
+package DoubleLinkedList.set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(0);
+        myDLL.append(1);
+        myDLL.append(2);
+        myDLL.append(3);
+
+        System.out.println("DLL before set():");
+        myDLL.printList();
+
+        myDLL.set(2, 99);
+
+        System.out.println("\nDLL after set():");
+        myDLL.printList();
+
+
+
+
+    }
+
+}
