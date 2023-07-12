3659986a0969f5c1b3d565f5949a4d55a942835f
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-07-12 18:56:06 +0300

    Add files via upload

---
+++ src/main/java/Trees/constructor/BinarySearchTree.java
@@ -0,0 +1,15 @@
+package Trees.constructor;
+
+public class BinarySearchTree {
+    public Node root;
+
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+}
